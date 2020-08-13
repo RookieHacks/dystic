9406c8454cf4a8fa0f5badb7b4029473f4f6581b
andytubeee <andy.tubeee@gmail.com>
2020-08-13 17:25:57 -0400

    saved jobs database is real time deployed

--- public/app.js
+++ public/app.js
@@ -11,6 +11,8 @@ const userIDObj = document.getElementById('userid');
 
 const signOutmsg = document.getElementById('signoutmsg');
 
+const savedJobsSection = document.getElementById('saved-jobs-section');
+
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
 signInBtn.onclick = () => {
@@ -40,6 +42,7 @@ auth.onAuthStateChanged((user) => {
     signedOut.hidden = true;
     usernameObj.innerHTML = 'Greetings, ' + user.displayName + '!';
     userIDObj.innerHTML = 'User ID: ' + user.uid;
+    savedJobsSection.hidden = false;
   } else {
     // not signed in
     signedIn.hidden = true;
@@ -47,5 +50,46 @@ auth.onAuthStateChanged((user) => {
     usernameObj.innerHTML = '';
     userIDObj.innerHTML = '';
     signOutmsg.innerHTML = 'Signed out Successfully!';
+    savedJobsSection.hidden = true;
+  }
+});
+
+// Firebase firestore
+const db = firebase.firestore();
+const addJobBtn = document.getElementById('addJobBtn');
+const jobList = document.getElementById('saved-jobs');
+const jobInput = document.getElementById('newJobtxt');
+
+let jobsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    jobsRef = db.collection('saved-jobs');
+
+    try {
+      addJobBtn.onclick = () => {
+        const { serverTimestamp } = firebase.firestore.FieldValue;
+        boolVal = [true, false];
+
+        jobsRef.add({
+          uid: user.uid,
+          title: jobInput.value,
+          createdAt: serverTimestamp(),
+          isMatch: boolVal[Math.floor(Math.random() * boolVal.length)],
+        });
+      };
+    } catch (err) {
+      alert('Error Occurred: ' + err.message);
+    }
+
+    unsubscribe = jobsRef
+      .where('uid', '==', user.uid)
+      .onSnapshot((querySnapshot) => {
+        const jobTitleItems = querySnapshot.docs.map((doc) => {
+          return `<li>${doc.data().title}</li>`;
+        });
+        jobList.innerHTML = jobTitleItems.join('');
+      });
   }
 });
